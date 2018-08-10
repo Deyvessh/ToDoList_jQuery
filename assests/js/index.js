38013889ad803d5062cc13f1b4ce607f24e8a4b5
Deyvessh <deyvesshkumar@gmail.com>
2018-08-10 18:30:48 +0530

    adding removing_to-dolist

--- assests/js/index.js
+++ assests/js/index.js
@@ -3,3 +3,9 @@
 $("li").click(function() {
     $(this).toggleClass("color_lineThrough");
 });
+
+$("span").click(function() {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+});
