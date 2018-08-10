3952a7da95509b71d5cf8bbe6eaed9d85becf603
Deyvessh <deyvesshkumar@gmail.com>
2018-08-10 20:59:18 +0530

    finializing

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,11 +1,19 @@
-
-
-$("li").click(function() {
-    $(this).toggleClass("color_lineThrough");
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("color_lineThrough");
 });
 
-$("span").click(function() {
-    $(this).parent().fadeOut(500, function() {
-        $(this).remove();
-    });
+$("ul").on("click", "span", function (event) {
+	$(this).parent().fadeOut(500,function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function (event) {
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li> <span><i class='fa fa-trash'></i> </span> " + todoText + "</li>")
+	}
+});
+
